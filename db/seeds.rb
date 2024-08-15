dcfbc0c5990589e1396bbf4fc053af2f8a556101
valentin corger <valentin.corger@gmail.com>
2024-08-15 14:17:51 +0200

    frontend suite index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Destroying all bookings, reviews, rooms and users..."
+Room.destroy_all
+User.destroy_all
+puts "Creating a user..."
 User.create({
   :email => "valentin.corger@gmail.com",
   :password => "password"
 })
+
+puts "Creating rooms..."
+Room.create({
+  :name => "Gite Jonquille",
+  :description => "Chambre double avec salle de bain privée",
+  :price_per_day => 50.0,
+  :average_rating => 4.5
+})
+
+Room.create({
+  :name => "Gite Lila",
+  :description => "Chambre double avec salle de bain privée",
+  :price_per_day => 75.0,
+  :average_rating => 4.0
+})
+
+Room.create({
+  :name => "Gite Acacia",
+  :description => "Chambre double avec salle de bain privée",
+  :price_per_day => 100.0,
+  :average_rating => 4.5
+})
+
+Room.create({
+  :name => "Gite Glycine",
+  :description => "Chambre double avec salle de bain privée",
+  :price_per_day => 125.0,
+  :average_rating => 4.0
+})
+
+Room.create({
+  :name => "Gite Mimosa",
+  :description => "Chambre double avec salle de bain privée",
+  :price_per_day => 150.0,
+  :average_rating => 4.5
+})
+puts "Seeding done!"
